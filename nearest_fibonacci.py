2f736c67db0aba41e034c572bcb1af5a98289190
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2022-12-05 14:25:00 +0530

    def pal(n):     a=0     b=1     c=a+b     f=0     t=0     while t!=n:         if a==n:            f=1            break         a=b         b=c         c=a+b         t+=1     if f==1:         return True     else:         return False
    n=int(input())
    np=0
    pp=n+1
    for i in range(n-1,0,-1):
        if pal(i):
            np=i
            break
    while pal(pp)==False:
        pp+=1
    if pp-n<n-np:
        print(pp)
    elif pp-n>n-np:
        print(np)
    else:
        print(np,pp)

---
+++ nearest_fibonacci.py
@@ -0,0 +1,34 @@
+def pal(n):
+    a=0
+    b=1
+    c=a+b
+    f=0
+    t=0
+    while t!=n:
+        if a==n:
+           f=1
+           break
+        a=b
+        b=c
+        c=a+b
+        t+=1
+    if f==1:
+        return True
+    else:
+        return False
+
+n=int(input())
+np=0
+pp=n+1
+for i in range(n-1,0,-1):
+    if pal(i):
+        np=i
+        break
+while pal(pp)==False:
+    pp+=1
+if pp-n<n-np:
+    print(pp)
+elif pp-n>n-np:
+    print(np)
+else:
+    print(np,pp)
