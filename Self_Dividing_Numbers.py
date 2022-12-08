c411ccc324fbe21ddac034f350afe55a268bcbe5
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2022-12-08 13:32:12 +0530

    def s(n):     m=n     d=0     f=0     while n!=0:         d=n%10         if d==0 or m%d!=0:             f=1             break         n=n//10     if f==0:         return True     else:         return n=int(input()) m=int(input()) for i in range(n,m+1):     if s(i):         print(i,end=' ')

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,16 +1,19 @@
+def s(n):
+    m=n
+    d=0
+    f=0
+    while n!=0:
+        d=n%10
+        if d==0 or m%d!=0:
+            f=1
+            break
+        n=n//10
+    if f==0:
+        return True
+    else:
+        return
 n=int(input())
 m=int(input())
 for i in range(n,m+1):
-    k=i
-    c=0
-    d=0
-    while k!=0:
-        d=k%10
-        k=k//10
-        if d!=0 and i%d==0:
-            c=0
-        else:
-            c=1
-            break
-    if c==0:
+    if s(i):
         print(i,end=' ')
