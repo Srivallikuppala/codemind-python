df849adc3c514eb1acbd42937442e369d8dfa4fb
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2022-12-08 12:27:19 +0530

    def pr(n):     if n==1:         return False     for i in range(2,int(pow(n,0.5))+1):         if n%i==0:             return False     else:         return True n=int(input()) m=int(input()) c=0 for i in range(n,m+1):     if pr(i):         c+=1 print(c)

---
+++ Primes_in_range.py
@@ -0,0 +1,15 @@
+def pr(n):
+    if n==1:
+        return False
+    for i in range(2,int(pow(n,0.5))+1):
+        if n%i==0:
+            return False
+    else:
+        return True
+n=int(input())
+m=int(input())
+c=0
+for i in range(n,m+1):
+    if pr(i):
+        c+=1
+print(c)
