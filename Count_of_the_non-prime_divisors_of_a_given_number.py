8d3482df4852a9caf69d317489d51f079cf38518
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2022-12-08 12:17:15 +0530

    def pr(n):     if n==1:         return False     for i in range(2,int(pow(n,0.5))+1):         if n%i==0:             return False     return True n=int(input()) c=0 for i in range(1,n+1):     if n%i==0 and pr(i)==False:         c+=1 print(c)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,17 +1,13 @@
-def is_p(n):
-    c=0
-    for i in range(1,n+1):
-        if n%i==0:
-            c+=1
-    if c==2:
+def pr(n):
+    if n==1:
         return False
-    else:
-        return True
+    for i in range(2,int(pow(n,0.5))+1):
+        if n%i==0:
+            return False
+    return True
 n=int(input())
-c1=0
+c=0
 for i in range(1,n+1):
-    if n%i==0:
-        
-        if is_p(i):
-            c1+=1
-print(c1)
+    if n%i==0 and pr(i)==False:
+        c+=1
+print(c)
