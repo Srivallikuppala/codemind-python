60400c3330f8b1fd5119f016d7a1b3bbebf58889
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2022-12-14 15:45:47 +0530

    n=int(input()) sum=0 for i in range(1,(n//2)+1):     if(n%i==0):         sum=sum+i if(sum==n):     print("True") else:     print("False")

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,13 +1,9 @@
-def div(x):
-    s=0
-    for i in range (1,x):
-        if(x%i==0):
-            s=s+i
-    return s
-
-x=int(input())
-d=div(x)
-if x==d:
+n=int(input())
+sum=0
+for i in range(1,(n//2)+1):
+    if(n%i==0):
+        sum=sum+i
+if(sum==n):
     print("True")
 else:
     print("False")
