7858d25d596479b4293fdeab9057bacd9ce5a5f0
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2023-07-03 12:14:14 +0530

    n=input() c1=0 c2=0 for i in n:     if(ord(i)==122):         c1=c1+1     elif(ord(i)==111):         c2=c2+1 if(c2==c1*2):     print("Yes") else:     print("No")

--- Zoos.py
+++ Zoos.py
@@ -1,7 +1,12 @@
 n=input()
-x=n.count('z')
-y=n.count('o')
-if x*2==y:
+c1=0
+c2=0
+for i in n:
+    if(ord(i)==122):
+        c1=c1+1
+    elif(ord(i)==111):
+        c2=c2+1
+if(c2==c1*2):
     print("Yes")
 else:
     print("No")
