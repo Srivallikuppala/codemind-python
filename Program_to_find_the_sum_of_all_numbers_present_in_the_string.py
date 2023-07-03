c707a5fc336d31dc1960da15b031ffa5cbd157c8
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2023-07-03 12:02:57 +0530

    n=input() sum='' k=0 for i in n:     if ord(i)>47 and ord(i)<58:         sum=sum+i s=int(sum) while s>0:     d=s%10     k=k+d     s=s//10 print(k)

--- Program_to_find_the_sum_of_all_numbers_present_in_the_string.py
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.py
@@ -1,7 +1,12 @@
-a=input()
-b=list(a)
-c=0
-for i in b:
-    if i.isdigit():
-        c+=int(i)
-print(c)
+n=input()
+sum=''
+k=0 
+for i in n:
+    if ord(i)>47 and ord(i)<58:
+        sum=sum+i
+s=int(sum)
+while s>0:
+    d=s%10
+    k=k+d
+    s=s//10
+print(k)
