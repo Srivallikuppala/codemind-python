46d5d0d32bc3de35fedc5513cb857f29255c0e56
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2023-07-04 10:26:50 +0530

    n=input() k=input() c=0 flag=0 for i in n:     if(i==k):         c=c+1         flag=1 if(flag==1):     print(c) else:     print(-1)

--- counting_the_occurances.py
+++ counting_the_occurances.py
@@ -1,7 +1,12 @@
 n=input()
-m=input()
-r=n.count(m)
-if r>0:
-    print(r)
+k=input()
+c=0
+flag=0
+for i in n:
+    if(i==k):
+        c=c+1
+        flag=1
+if(flag==1):
+    print(c)
 else:
-    print('-1')
+    print(-1)
