12722a8c3296e5ceb92a40cd42f54e08ee9d60d9
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2023-07-04 10:45:38 +0530

    t=int(input()) for j in range(t):     n=input()     k=0     for i in n:         if (ord(i)<48 or ord(i)>57):             k=1             break     if(k!=1):         print("True")     else:         print("False")

--- String_decimal_.py
+++ String_decimal_.py
@@ -1,7 +1,12 @@
-n=int(input())
-for i in range(n):
-    x=input()
-    if x.isdigit():
-        print(True)
+t=int(input())
+for j in range(t):
+    n=input()
+    k=0
+    for i in n:
+        if (ord(i)<48 or ord(i)>57):
+            k=1
+            break
+    if(k!=1):
+        print("True")
     else:
-        print(False)
+        print("False")
